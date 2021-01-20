6491ba724ac8980eeac63379b99ea97477bedb1a
CaptDread <domlycaon@gmail.com>
2021-01-20 17:10:52 -0500

    sepuku

--- src/js/dreadScript/logo.js
+++ src/js/dreadScript/logo.js
@@ -1,11 +1,2 @@
-"use strict"; // This file is specificly meant to control the Logo animations
-
-var logo = document.querySelector('.dreadlogo');
-var logoLink = document.querySelector(".logoLink");
-logoLink.addEventListener("mouseover", function () {
-  logo.src = 'src/js/dreadScript/logoClose.png';
-});
-logoLink.addEventListener("mouseout", function () {
-  logo.src = 'src/js/dreadScript/logoOpen.png';
-});
+"use strict";
 //# sourceMappingURL=logo.js.map
