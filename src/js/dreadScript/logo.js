72fd70b5060fe28633ced09c64b83cd1a86a8b8e
CaptDread <domlycaon@gmail.com>
2021-01-21 15:31:41 -0500

    moreMixinFixin

--- src/js/dreadScript/logo.js
+++ src/js/dreadScript/logo.js
@@ -1,12 +1,11 @@
-let logo = document.querySelector('.dreadlogo')
-let logoLink = document.querySelector(`.logoLink`)
+"use strict";
 
-logoLink.addEventListener(`mouseover`, function(){
-    logo.src = 'src/js/dreadScript/logoClose.png'
-})
-
-logoLink.addEventListener(`mouseout`, function(){
-    logo.src = 'src/js/dreadScript/logoOpen.png'
-})
-
+var logo = document.querySelector('.dreadlogo');
+var logoLink = document.querySelector(".logoLink");
+logoLink.addEventListener("mouseover", function () {
+  logo.src = 'src/js/dreadScript/logoClose.png';
+});
+logoLink.addEventListener("mouseout", function () {
+  logo.src = 'src/js/dreadScript/logoOpen.png';
+});
 //# sourceMappingURL=logo.js.map
